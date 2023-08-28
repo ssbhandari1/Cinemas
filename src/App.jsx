8fecd8c2475fb90046cbba8ee67dfd2e7cea5797
santosh <santoshbirkhal@gmail.com>
2023-08-28 23:09:58 +0530

    crasoul

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import './App.css'
 import { fetchdataFromApi } from './utils/api'
-import { useDispatch } from 'react-redux'
-import { getApiConfiguration } from './redux/homeSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import { getApiConfiguration, getGenres } from './redux/homeSlice'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/home/Home'
 import Details from './pages/details/Details'
@@ -11,12 +11,17 @@ import Explore from './pages/explore/Explore'
 import PNFound from './pages/404/PNFound'
 import Header from './components/Header'
 import Footer from './components/Footer'
+
+
+
 const App = () => {
+  const {url}=useSelector((state)=>state.home)
 const dispatch=useDispatch()
 
 
   useEffect(()=>{
 fetchApiConfig()
+genresCall()
   },[])
 
   const fetchApiConfig=async()=>{
@@ -35,6 +40,24 @@ profile:res.images.secure_base_url + "original"
     }
   }
 
+
+  const genresCall=async()=>{
+    let promises=[]
+    let endPoints=['tv','movie']
+    let allGenres={}
+
+    endPoints.forEach((url)=>{
+      promises.push(fetchdataFromApi(`/genre/${url}/list`))
+    })
+
+    const data=await Promise.all(promises)
+  
+    data.map(({genres})=>{
+      return genres.map((item)=>(allGenres[item.id]=item))
+    })
+    
+    dispatch(getGenres(allGenres))
+  }
   return (
     <BrowserRouter>
     <Header/>
@@ -45,7 +68,7 @@ profile:res.images.secure_base_url + "original"
 <Route path='/explore/:mediaType' element={<Explore/>}/>
 <Route path='/*' element={<PNFound/>}/>
   </Routes>
-  {/* <Footer/> */}
+  <Footer/>
   </BrowserRouter>
   )
 }
